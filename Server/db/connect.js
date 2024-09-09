f01397851f351783ebc551310fb59721d685e2e4
Atharva Khode <atharvakhode3@gmail.com>
2024-09-09 23:56:22 +0530

    modified connect.js

--- Server/db/connect.js
+++ Server/db/connect.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = () => {
   return mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
 }
 
