86f844b40dd84756e83dcf86810ed507b0c262fd
Atharva Khode <atharvakhode3@gmail.com>
2024-09-09 23:54:20 +0530

    modified connect.js

--- Server/db/connect.js
+++ Server/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
+const connectDB = () => {
+  return mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
