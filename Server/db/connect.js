74e93807262039750c267f08d453a4193f80a16b
Atharva Khode <atharvakhode3@gmail.com>
2024-09-10 00:20:35 +0530

    modified connect.js to console connection status

--- Server/db/connect.js
+++ Server/db/connect.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose')
+const connectDB = async () => {
+  try {
+      await mongoose.connect(process.env.MONGO_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      });
+      console.log("MongoDB connected successfully");
+  } catch (error) {
+      console.error("MongoDB connection failed", error);
+  }
+};
 
-const connectDB = () => {
-  return mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-}
-
-module.exports = connectDB
+module.exports = connectDB;
