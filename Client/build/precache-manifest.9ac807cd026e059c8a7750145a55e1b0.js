63161422bd7d25a3dc7827c2c7a3ffda466f663f
Atharva Khode <atharvakhode3@gmail.com>
2024-09-16 17:36:45 +0530

    build folder

---
+++ Client/build/precache-manifest.9ac807cd026e059c8a7750145a55e1b0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af41eb7b4d98e9e0cf99362261f02975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4dca8fec6609d6263eb",
+    "url": "/static/css/2.61989ea4.chunk.css"
+  },
+  {
+    "revision": "b4345e9d7a0433b430a6",
+    "url": "/static/css/main.db28ec1e.chunk.css"
+  },
+  {
+    "revision": "a4dca8fec6609d6263eb",
+    "url": "/static/js/2.b6507ea8.chunk.js"
+  },
+  {
+    "revision": "e5bc5cd54f29f6988cb435ae26b098fb",
+    "url": "/static/js/2.b6507ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f891daabf1d39a9cbde",
+    "url": "/static/js/3.ff4e2d70.chunk.js"
+  },
+  {
+    "revision": "b4345e9d7a0433b430a6",
+    "url": "/static/js/main.a394b29b.chunk.js"
+  },
+  {
+    "revision": "871102f988ef7b6f147c",
+    "url": "/static/js/runtime-main.3f888405.js"
+  },
+  {
+    "revision": "3954ec8019c45e68f1df65d73edfd933",
+    "url": "/static/media/LoginPageImg.3954ec80.png"
+  },
+  {
+    "revision": "f795a0941353adce66e539774caf5681",
+    "url": "/static/media/PineVox-Horizontal-bw.f795a094.svg"
+  },
+  {
+    "revision": "9f6283f6692e5de8a53dcf4d74d8e26c",
+    "url": "/static/media/Pinevox-Horizontal.9f6283f6.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
